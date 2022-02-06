9cc5e92d90ab90950b9962e8220bfb9a7e08f814
Yuki326 <s20a1035@gmail.com>
2022-02-06 16:17:18 +0900

    関数コンポーネントに変更

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 // import Link from 'next/link'
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link'
 function joinRoom(num){
@@ -29,56 +29,41 @@ function joinRoom(num){
       });
   }
 
+
+
+ 
+export default function App() {
+    
   
-class App extends Component {
-    state = {
-        posts: []
-    };
-
-    componentDidMount() {
-        this.getPosts();
-        setInterval(() => {
-            axios
-
-            .get('http://numeronbackend.azurewebsites.net/getRooms')
-
-            .then(res => {
-                console.log(res.data.room)
-                this.setState({ posts: res.data.room });
-            })
-            .catch(err => {
-                console.log(err);
-            });
-          }, 3000);
-    }
-
-    getPosts() {
+    const [posts,setPosts] = useState([])
+    React.useEffect(function() {
+      const intervalId = setInterval(function() {
         axios
-
-            .get('http://localhost:8000/getRooms')
-
+            .get('http://numeronbackend.azurewebsites.net/getRooms?')
+  
             .then(res => {
-                console.log(res.data.room)
-                this.setState({ posts: res.data.room });
+                // console.log(res.data.room.num)
+                setPosts(res.data.room);
+                
             })
             .catch(err => {
                 console.log(err);
             });
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.posts.map(item => (
+      }, 1000);
+      return function(){clearInterval(intervalId)};
+    });
+    
+  
+    
+    return (
+      <div>
+        {posts.map(item => (
                      <div key={item.id}>
                         <Link href={"sample/wait?id="+item.id}><a onClick={()=>joinRoom(item.id)}>ルーム{item.id}</a></Link>
                         <p>{item.num}人</p>
                      </div>
                 ))}
                 <div onClick={createRoom}>部屋作成</div>
-            </div>
-        );
-    }
-}
-
-export default App;
+      </div>
+    )
+  }
